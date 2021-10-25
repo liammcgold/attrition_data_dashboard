80bc4f70091fd3c6696a9823da1a46ab0ec4d12e
liammcgold <31221321+liammcgold@users.noreply.github.com>
2021-10-24 23:42:01 -0500

    added new reqs

--- data_app.py
+++ data_app.py
@@ -1,7 +1,7 @@
 import dash
 from dash.dependencies import Input, Output
-import dash_core_components as dcc
-import dash_html_components as html
+from dash import html
+from dash import dcc
 from dash import dash_table
 import pandas as pd
 import ModelSavingLogic
